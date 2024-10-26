4bfe4dfcc31b1f84dec0e666b6eed997c7ced0c2
Anagha <anaghabj53@yahoo.com>
2024-10-25 22:41:03 -0400

    commit for deliverable 3

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from datetime import datetime
 # import hashlib
 
 app = Flask(__name__)
-
-# app.secret_key = ""
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:course123@localhost/spotsalon"
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
 
@@ -15,55 +13,64 @@ Base.metadata.create_all(engine, checkfirst=True)
 
 @app.route('/', methods=['post','get'])
 def index():
+
     msg = ''
     if request.method == 'post' and 'username' in request.form and 'pwd' in request.form:
-        username = request.form['username']
-        password = request.form['pwd']
-        with engine.connect() as con:
-            result = con.execute(text(f"select * from profile where email_id = '{username}' and password = '{password}' "))
-            account = result.fetchone()
-            con.commit()
+        try:
+            username = request.form['username']
+            password = request.form['pwd']
+            with engine.connect() as con:
+                result = con.execute(text(f"select * from profile where email_id = '{username}' and password = '{password}' "))
+                account = result.fetchone()
+                con.commit()
 
-        if account:
-            session['loggedin'] = True
-            session['id'] = account.id
-            session['username'] = account.username
-            msg = 'logged in successfully'
-            return redirect(url_for('home_business', msg = msg))
-        else:
-            msg = 'incorrect username or password'
-    return render_template('index.html', msg = msg)
+            if account:
+                session['loggedin'] = True
+                session['id'] = account.id
+                session['username'] = account.username
+                msg = 'logged in successfully'
+                return redirect(url_for('home_business', msg = msg))
+        except Exception as e:
+            print(e)
+            return render_template('index.html', e=e)
+    return render_template('index.html')
 
 
 
 @app.route('/registration_business', methods = ['post', 'get'])
 def registration_business():
     msg = ""
-    if request.method == 'post' and 'username' in request.form and 'pwd' in request.form:
-        # Get the form values
-        role_id = request.form['role_id']
-        name = request.form['businessname']
-        email = request.form['email'].lower()
-        password = request.form['pwd']
-        cpassword = request.form['cpwd']
-        phone = request.form['phone']
-        if password != cpassword:
-            msg = 'password do not match'
-            return render_template('registration_business.html', msg = msg )
-        with engine.connect() as con:
-            result = con.execute(text(f"select * from profile where email_id == '{email}' and password == '{password}' "))
-            account = result.fetchone()
-            con.commit()
-
-            if account:
-                msg = 'account already exists'
-                return render_template('registration_business.html', msg = msg)
-            else:
-                #insert the user into database
-                con.execute(text(f"insert into profile (role_id, name, email_id, password, phone) values( '{role_id}', '{name}', '{email}', '{password}', '{phone}' )"))
+    if request.method == 'POST':
+        try:
+            # Get the form values
+            role = request.form['role']
+            name = request.form['businessname']
+            email = request.form['email'].lower()
+            password = request.form['pwd']
+            cpassword = request.form['cpwd']
+            if password != cpassword:
+                msg = 'password do not match'
+                return render_template('registration_business.html', msg = msg )
+       
+            #check if account already exists
+            with engine.connect() as con:
+                result = con.execute(text(f"select * from profile where email_id = '{email}' and password = '{password}' "))
+                account = result.fetchone()
                 con.commit()
-            msg = 'account created successfully'
-            return redirect(url_for('home_business', msg=msg))
+
+                if account:
+                    msg = 'Account already exists'
+                    return render_template('registration_business.html', msg = msg)
+                else:
+                #insert the user into database
+                    with engine.connect() as con:
+                        con.execute(text(f"insert into profile (role, name, email_id, password) values( '{role}', '{name}', '{email}', '{password}' )"))
+                        con.commit()
+                        msg = 'account created successfully'
+                    return render_template('home_business.html', msg=msg)
+        except Exception as e:
+            print(e)
+            return render_template('registration_business.html', e=e)
     return render_template('registration_business.html')
 
 @app.route('/about')
@@ -116,8 +123,60 @@ def signin_admin():
             msg = 'incorrect username or password'
     return render_template('signin_admin.html', msg = msg)
 
+# This page will feature a form for updating the business account, 
+# including contact information and options to select the various services offered by the business.
+# To edit Account Information
 @app.route('/home_business', methods = ['get', 'post'])
 def home_business():
+    account = ""
+    if request.method == 'POST':
+        try:
+            # Get the form values for table profile
+            spot_id = request.form['spot_id']
+            role = request.form['role']
+            name = request.form['businessname']
+            username = request.form['email'].lower()
+            password = request.form['pwd']
+
+            # Get the form values for table salon_contact
+            phone = request.form['phone']
+            city = request.form['city']
+            state = request.form['state']
+            country = request.form['country']
+            address = request.form['address']
+            open_time = request.form['open_time']
+            close_time = request.form['close_time']
+
+            # Get the form values for table salon_services
+            hair = request.form['hair']
+            skin = request.form['skin']
+            nail = request.form['nail']
+            massage = request.form['massage']
+            wax = request.form['wax']
+            makeup = request.form['makeup']
+            with engine.connect() as con:
+                result = con.execute(text(f"select * from profile where email_id = '{username}' and password = '{password}' "))
+                account = result.fetchone()
+                con.commit()
+                if account:
+                    session['loggedin'] = True
+                    session['id'] = account.id
+                    session['username'] = account.username
+                    return render_template('home_business.html', account = account)
+            
+            # Insert the values into table salon_contact & salon_services
+            with engine.connect() as con:
+                spot = con.execute(text(f"select spot_id from profile where email_id = '{username}' "))
+                con.execute(text(f"insert into salon_contact (spot_id,phone,city,state,country,address,opening_time,closing_time) \
+                                  values ('{spot}', '{phone}','{city}','{state}', '{country}', '{address}', '{open_time}', '{close_time}'\
+                                      ) where spot_id = '{spot}'"))
+                con.execute(text(f"insert into salon_services (spot_id,hair,skin,nails,massage,wax,makeup) \
+                                 values ( '{spot}', '{hair}', '{skin}', '{nail}', '{massage}', '{wax}', '{makeup}' ) \
+                                    where spot_id = '{spot}'"))
+                con.commit()
+        except Exception as e:
+            print(e)
+            return render_template('signin_business_account.html', e=e)
     return render_template('home_business.html')
 
 @app.route('/home_admin', methods = ['get', 'post'])
@@ -129,8 +188,6 @@ def customer_display():
     return render_template('customer_display.html')
 
 
-# This page will feature a form for updating the business account, 
-# including contact information and options to select the various services offered by the business.
 
 # @app.route('/home_business', methods = ['get', 'post'])
 # def home_business():
--- models/models.py
+++ models/models.py
@@ -16,24 +16,21 @@ class BaseModel:
 class Base(DeclarativeBase):
     pass
 
-class Roles(Base, BaseModel):
-    __tablename__ = 'roles'
-    role_id:Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    role:Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
 
 class Profile(Base, BaseModel):
     __tablename__ = 'profile'
     spot_id:Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    role_id:Mapped[int] = mapped_column(Integer, ForeignKey("roles.role_id", onupdate="CASCADE", ondelete="CASCADE"))
+    role:Mapped[str] = mapped_column(String(100), nullable=False)
     name:Mapped[str] = mapped_column(String(200), nullable=False)
     email_id:Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password:Mapped[str] = mapped_column(String(100),nullable=False)
-    phone:Mapped[int] = mapped_column(Integer, nullable=False)
+   
     
 class SalonContact(Base, BaseModel):
     __tablename__ = 'salon_contact'
     contact_id:Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key= True)
     spot_id:Mapped[int] = mapped_column(Integer, ForeignKey("profile.spot_id", onupdate="CASCADE", ondelete="CASCADE"))
+    phone:Mapped[int] = mapped_column(Integer, nullable=False)
     city:Mapped[str] = mapped_column(String(100), nullable=False)
     state:Mapped[Optional[str]] = mapped_column(String(100))
     country:Mapped[str] = mapped_column(String(100), nullable=False)
@@ -45,7 +42,6 @@ class SalonServices(Base, BaseModel):
     __tablename__ = 'salon_services'
     service_id:Mapped[int] = mapped_column(Integer, autoincrement= True, primary_key= True)
     spot_id:Mapped[int] = mapped_column(Integer, ForeignKey("profile.spot_id", onupdate="CASCADE", ondelete="CASCADE"))
-   # name:Mapped[str] = mapped_column(String(200), ForeignKey("profile.name", onupdate="CASCADE", ondelete="CASCADE"))
     hair:Mapped[Optional[str]] = mapped_column(String(50))
     skin:Mapped[Optional[str]] = mapped_column(String(50))
     nails:Mapped[Optional[str]] = mapped_column(String(50))
